e57896fea234e4acc0e596b35f8dd1f94e94b7de
Pawanesh <55928870+Pawaneshmishra@users.noreply.github.com>
2021-10-17 21:18:45 +0530

    Update HelloClient.java

--- RMI application/HelloClient.java
+++ RMI application/HelloClient.java
@@ -9,23 +9,22 @@ import java.util.Scanner;
  *
  */
 public class HelloClient {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			System.out.println("Client RMI stub is running...");
-			Hello stb = (Hello)Naming.lookup("rmi://localhost:1900/Hello");
-			Scanner scan = new Scanner(System.in);
-			String input;
-			System.out.print("Enter your message: ");
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+try {
+	System.out.println("Client RMI stub is running...");
+	Hello stb = (Hello)Naming.lookup("rmi://localhost:1900/Hello");
+	Scanner scan = new Scanner(System.in);
+	String input;
+	System.out.print("Enter your message: ");
 			
-			input = scan.nextLine();
-			System.out.println(stb.msg(input));
+	input = scan.nextLine();
+	System.out.println(stb.msg(input));
 			
-		} catch (MalformedURLException | RemoteException | NotBoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Stub error...");
-			e.printStackTrace();
+} catch (MalformedURLException | RemoteException | NotBoundException e) {
+	// TODO Auto-generated catch block
+	System.out.println("Stub error...");
+	e.printStackTrace();
 		}
 	}
-
 }

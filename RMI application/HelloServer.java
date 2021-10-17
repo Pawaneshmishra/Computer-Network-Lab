b524c07a1b8415b26a0b8686a4c3ee1cfe7b6e3c
Pawanesh <55928870+Pawaneshmishra@users.noreply.github.com>
2021-10-17 21:19:50 +0530

    Update HelloServer.java

--- RMI application/HelloServer.java
+++ RMI application/HelloServer.java
@@ -8,20 +8,19 @@ import java.rmi.registry.*;
  *
  */
 public class HelloServer {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			System.out.println("Hello RMI Server is running...");
-			Hello skltn = new HelloImpl();
-			Registry registry = LocateRegistry.createRegistry(1900);
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+try {
+	System.out.println("Hello RMI Server is running...");
+	Hello skltn = new HelloImpl();
+	Registry registry = LocateRegistry.createRegistry(1900);
 			
-			registry.rebind("Hello", skltn);
+	registry.rebind("Hello", skltn);
 			
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Server is not running.....");
-			e.printStackTrace();
+} catch (RemoteException e) {
+	// TODO Auto-generated catch block
+	System.out.println("Server is not running.....");
+	e.printStackTrace();
 		}
 	}
-
 }
